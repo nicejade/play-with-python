1bbff49ed1db63e7abb3efb700b34f14515d48eb
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2020-03-14 23:40:09 +0800

    :ambulance: yaml replace load to safe_load

--- automation/utils.py
+++ automation/utils.py
@@ -4,10 +4,9 @@ import yaml
 
 def get_config(field):
     path = os.path.join(".", "config.yaml")
-    print(path)
     file = open(path, "r", encoding="utf-8")
     file_data = file.read()
     file.close()
 
-    config = yaml.load(file_data)
+    config = yaml.safe_load(file_data)
     return config[field]
