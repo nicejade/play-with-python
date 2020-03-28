a2fa5d83d8d43f2c489866cad394e192db20c88b
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2020-03-29 01:50:32 +0800

    :bug: restore url truth path

--- scripts/find-break-urls.py
+++ scripts/find-break-urls.py
@@ -4,10 +4,18 @@ import sys
 import time
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
 from utils import *
 
 # 增加重连次数
 requests.adapters.DEFAULT_RETRIES = 5
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip,deflate,sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8",
+    "Connection": "close",
+}
 
 # 爬出来的所有链接
 all_urls_list = []
@@ -36,13 +44,6 @@ def write_urls_to_txt(urls_list, filename):
 
 
 def request_url(url):
-    HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip,deflate,sdch",
-        "Accept-Language": "zh-CN,zh;q=0.8",
-        "Connection": "close",
-    }
     try:
         req = requests.session()
         # Fix requests“Max retries exceeded with url” error
@@ -57,6 +58,7 @@ def request_url(url):
         break_urls_list.append(url)
 
 
+# 检查列表中 url，对为已经无法正常访问的 url 地址输出至 break_urls.txt
 def check_is_break(urls_list):
     counter = 0
     # 先将列表去重
@@ -72,6 +74,7 @@ def check_is_break(urls_list):
     write_urls_to_txt(break_urls_list, "break_urls.txt")
 
 
+# 递归遍历本文件，对所有非隐藏文件进行处理（read & check）
 def traverse_dir(path):
     # 得到文件夹下的所有文件名称
     files = os.listdir(path)
@@ -85,12 +88,30 @@ def traverse_dir(path):
                 read_deal_file(sub_path)
 
 
+# 爬取在线网页中的 url （a 标签中的 href）
+def spider_online_url(url_path):
+    resp = requests.get(url_path, headers=HEADERS, timeout=6)
+    if resp.status_code == 200:
+        htmlBody = resp.text
+        soup = BeautifulSoup(htmlBody, "lxml")
+        a_label_list = soup.find_all("a")
+        for item in a_label_list:
+            if item["href"] != "javascript:;":
+                real_path = restore_url_real_path(item["href"], url_path)
+                all_urls_list.append(real_path)
+    else:
+        print("此 Url 好像无法正常访问，请予以检查")
+
+
 if __name__ == "__main__":
     argvList = sys.argv
     specifiedPath = argvList[1]
-    fileIns = Path(specifiedPath)
-    if fileIns.is_dir():
-        traverse_dir()
+    if check_is_url(specifiedPath):
+        spider_online_url(specifiedPath)
     else:
-        read_deal_file(specifiedPath)
+        fileIns = Path(specifiedPath)
+        if fileIns.is_dir():
+            traverse_dir()
+        else:
+            read_deal_file(specifiedPath)
     check_is_break(all_urls_list)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,7 @@
 import re
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 
 # 检查是否为 url
 def check_is_url(string):
@@ -19,3 +20,15 @@ def find_all_urls(string):
     # findall() 查找匹配正则表达式的字符串
     links = re.findall("(https?\://[a-zA-Z0-9\-_\u4e00-\u9fa5\.\?/&\=\:]+)", string)
     return links
+
+
+# 对 url 是以 "/" 或 "//" 地址，恢复真是地址
+def restore_url_real_path(target_url, url):
+    parse_result = urlparse(url)
+    scheme = parse_result.scheme
+    netloc = parse_result.netloc
+    if target_url.startswith("//"):
+        return scheme + ":" + target_url
+    if target_url.startswith("/"):
+        return scheme + "://" + netloc + target_url
+    return target_url
